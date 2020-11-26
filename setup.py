faae6a634c16fe961b2cfdf5cb08bb59d0578b35
Jordan Rinder <rindjord@umich.edu>
2020-11-26 13:41:54 -0500

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name='pnews-jrinder42',
-  version='0.1.0',
-  description=long_description,
-  author='Jordan Rinder',
-  author_email='jordan.rinder@gmail.com',
-  packages=setuptools.find_packages(),
-  install_requires=[
+    name='pnews-jrinder42',
+    version='0.1.0',
+    description=long_description,
+    author='Jordan Rinder',
+    author_email='jordan.rinder@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=[
     "pyfiglet",
     "typing",
     "toml",
-    "feedparser"
+    "feedparser",
+    "sphinx_autodoc_typehints"
   ],
   python_requires='>=3.6',
   license='MIT',
